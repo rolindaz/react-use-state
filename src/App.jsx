641873235e5cc03822ddff7eac47f0104149473e
rolindaz <rolinda.zamboni@gmail.com>
2025-07-22 14:14:31 +0200

    Add initial folders and files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/*
+Consegna:
+Dato un array di oggetti contenente i linguaggi del web e le loro descrizioni:
+Crea una lista di bottoni, uno per linguaggio.
+Inserisci sotto una card che mostri il titolo e la descrizione del primo linguaggio nell’array.
+Fare in modo che, cliccando uno dei bottoni, la card in basso visualizzi il linguaggio corrispondente e la relativa descrizione
+*/
+
+import { useState } from 'react'
+import progLanguages from './data/progLanguages'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/data/progLanguages.js
@@ -0,0 +1,64 @@
+const progLanguages = [
+    {
+        id: 1,
+        name: "SQL",
+        description: "Used for managing and querying data in relational databases, essential for data analysis and business intelligence."
+    },
+    {
+        id: 2,
+        name: "JavaScript",
+        description: "Primarily used for web development, enabling interactivity on websites and powering front - end frameworks like React, Angular, and Vue."
+    },
+    {
+        id: 3,
+        name: "R",
+        description: "A language and environment specifically designed for statistical computing and graphics, widely used in data science and research."
+    },
+    {
+        id: 4,
+        name: "PHP",
+        description: "A server - side scripting language commonly used for web development, particularly for creating dynamic web pages."
+    },
+    {
+        id: 5,
+        name: "HTML/CSS",
+        description: "Markup languages used for structuring and styling web pages, not considered programming languages in the traditional sense but essential for web development."
+    },
+    {
+        id: 6,
+        name: "Typescript",
+        description: "A superset of JavaScript that adds static typing, improving code maintainability and scalability for larger projects."
+    },
+    {
+        id: 7,
+        name: "Matlab",
+        description: "Used for numerical computing, data analysis, and algorithm development."
+    },
+    {
+        id: 8,
+        name: "Kotlin",
+        description: "A modern language that runs on the Java Virtual Machine, increasingly popular for Android development."
+    },
+    {
+        id: 9,
+        name: "Rust",
+        description: "A systems programming language focusing on memory safety and concurrency, used for building high - performance applications."
+    },
+    {
+        id: 10,
+        name: "Python",
+        description: "Known for its readability and versatility, used in web development, data science, machine learning, and scripting."
+    },
+    {
+        id: 11,
+        name: "Java",
+        description: "A robust, object-oriented language popular for enterprise applications, Android development, and large-scale systems."
+    },
+    {
+        id: 12,
+        name: "C++",
+        description: "A powerful language that combines object-oriented and procedural programming, widely used for system programming, game development, and high-performance applications"
+    }
+]
+
+export default progLanguages
